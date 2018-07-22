ab0bc1ab14c623fd119251cfb3396e706f1c8a6a
shayanrizvi <sshayanrizvi@gmail.com>
2018-07-22 15:37:18 -0500

    added tabulation mode

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -6,6 +6,7 @@ import java.util.*;
  *
  * This program takes a user entered integer n between 1 and 92 and returns the nth fibonacci number.
  * The user can toggle between dynamic and normal mode to get the fibonacci number either dynamically or normally.
+ * The user can also toggle between a top-down memoization approach and a bottom-up tabulation approach.
  * 
  */
 public class Fibonacci {
@@ -18,6 +19,7 @@ public class Fibonacci {
 		// initialize variables
 		boolean q = false;														// program state
 		boolean d = false;														// dynamic/normal mode
+		boolean t = false;														// tabulation/memoization mode
 		Scanner in = new Scanner(System.in);									// input scanner
 		String input;															// user input
 		
@@ -29,17 +31,25 @@ public class Fibonacci {
 			
 			System.out.println("Enter an integer between 1 and 92");			// input prompt
 			System.out.println("Enter \"d\" to toggle between dynamic and normal mode");
+			System.out.println("Enter \"t\" to toggle between tabulation and memoization mode");
+			System.out.println("Enter \"c\" to clear fibonacci sequence memory: ");
 			System.out.println("Enter \"q\" to quit: ");
 			System.out.println();												// print new line
 					
 			input = in.nextLine();												// record user input
 			System.out.println();												// print new line
 			
+			// check for command input
 			if(input.equals("q")) {
 				
-				q = true;
-				System.out.println("Program terminated.");						// program termination prompt
-
+				q = true;														// change program state to termination
+				System.out.println("Program terminated.");						// program termination message
+				
+			} else if (input.equals("c")) {
+				
+				fibList = new long[0];											// clear fibonacci sequence list
+				System.out.println("Fibonacci sequence memory cleared.");		// display list cleared message
+				
 			} else if (input.equals("d")) {
 				
 				d = !(d);														// toggle dynamic/normal mode
@@ -47,7 +57,13 @@ public class Fibonacci {
 				if (d) System.out.println("Dynamic mode");						// check and display mode
 				else System.out.println("Normal mode");
 				
-				System.out.println();
+			} else if (input.equals("t")) {
+				
+				d = true;														// set mode to dynamic
+				t = !(t);														// toggle tabulation/memoization mode
+				
+				if (t) System.out.println("Tabulation mode");					// check and display mode
+				else System.out.println("Memoization mode");
 				
 			} else {
 				
@@ -65,17 +81,20 @@ public class Fibonacci {
 				// check for valid input range
 				if(0 < n && n < 93) {
 					
-					if(d) f = dFib(n);											// dynamically get fibonacci number
-					else f = fib(n);											// get fibonacci number
+					if(d) {
+						
+						if(t) f = tFib(n);										// get fibonacci number - tabulation
+						else f = mFib(n);										// get fibonacci number - memoization
+						
+					} else f = fib(n);											// get fibonacci number - normal
 					
 					System.out.println("fib(" + n + ") = " + f);				// print results
 					
 				} else System.out.println("Invalid input.");					// invalid input prompt
 				
-				System.out.println();											// print new line
-				
 			}
-
+			
+			System.out.println();												// print new line
 			System.out.println("# of method calls: " + c);						// print method call count
 			System.out.println("Fibonacci list: " + Arrays.toString(fibList));	// print fibonacci number list
 			System.out.println();												// print new line
@@ -103,15 +122,15 @@ public class Fibonacci {
 		else return fib(n-1) + fib(n-2);										// return sum of previous two fibonacci numbers
 		
 	}
-	
+
 	/**
 	 * Takes a positive integer n and returns the nth fibonacci number.
-	 * This method is dynamic.
+	 * This method is dynamic and uses a top-down memoization approach.
 	 * 
 	 * @param n fibonacci index to calculate
 	 * @return nth fibonacci number
 	 */
-	public static long dFib(int n) {
+	public static long mFib(int n) {
 		
 		c++;																	// increment method call count
 		
@@ -120,10 +139,40 @@ public class Fibonacci {
 			
 			if(fibList[n-1] != 0) return fibList[n-1];							// check if nth index is not 0
 			
-		} else fibList = Arrays.copyOf(fibList, n);								// copy fibonacci number list to new list with n indexes
+		} else fibList = Arrays.copyOf(fibList, n);								// copy fibonacci number list to new list with n indices
 		
 		if(n < 3) return fibList[n-1] = 1;										// return 1 for 1 and 2
-		else return fibList[n-1] = dFib(n-1) + dFib(n-2);						// store and return sum of previous two fibonacci numbers
+		else return fibList[n-1] = mFib(n-1) + mFib(n-2);						// store and return sum of previous two fibonacci numbers
+		
+	}
+
+	/**
+	 * Takes a positive integer n and returns the nth fibonacci number.
+	 * This method is dynamic and uses a bottom-up tabulation approach.
+	 * 
+	 * @param n fibonacci index to calculate
+	 * @return nth fibonacci number
+	 */
+	public static long tFib(int n) {
+		
+		c++;																	// increment method call count
+		
+		// check if fibonacci number is not stored for index n
+		if(fibList.length < n) {
+			
+			fibList = Arrays.copyOf(fibList, n);								// copy fibonacci number list to new list with n indices
+			
+			// for the number of indices up to n
+			for(int i = 0; i < n; i++) {
+				
+				if(i < 2) fibList[i] = 1;										// initialize first two fibonacci numbers
+				else fibList[i] = fibList[i-1] + fibList[i-2];					// calculate and store the fibonacci number for the index
+				
+			}
+		
+		}
+		
+		return fibList[n-1];													// return f(n)
 		
 	}
 	
